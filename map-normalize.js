a2bea0c726ab0a87bc160f2a42daf8cfb854661e
remy <remyhunter256@gmail.com>
2021-07-06 20:35:00 -0400

    Fix node module syntax

--- map-normalize.js
+++ map-normalize.js
@@ -5,6 +5,8 @@
  * 
 */
 
-module.exports = map(x, in_min, in_max, out_min, out_max)  {
+function map(x, in_min, in_max, out_min, out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+module.exports = map
